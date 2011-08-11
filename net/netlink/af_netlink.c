8134e2d4fcbb1f92f8eb111b08dcc99f9fc46c2c
Eric Dumazet <eric.dumazet@gmail.com>
2011-08-11 19:30:52 +0000

    net: cleanup some rcu_dereference_raw
    RCU api had been completed and rcu_access_pointer() or
    rcu_dereference_protected() are better than generic
    rcu_dereference_raw()
    
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 33d480ce6d43326e2541fd79b3548858a174ec3c)
    
    Conflicts:
    	net/netlink/af_netlink.c

--- net/netlink/af_netlink.c
+++ net/netlink/af_netlink.c
@@ -1596,7 +1596,7 @@ int __netlink_change_ngroups(struct sock *sk, unsigned int groups)
 		new = kzalloc(sizeof(*new) + NLGRPSZ(groups), GFP_ATOMIC);
 		if (!new)
 			return -ENOMEM;
-		old = nl_deref_protected(tbl->listeners);
+		old = rcu_dereference_protected(tbl->listeners, 1);
 		memcpy(new->masks, old->masks, NLGRPSZ(tbl->groups));
 		rcu_assign_pointer(tbl->listeners, new);
 
