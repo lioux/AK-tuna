bd125032b84037959b45f32a783427e59964262a
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-21 06:54:57 +0100

    dhd_linux.c: Fix Power mode compile error

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -567,9 +567,7 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
-#if !defined(SUPPORT_PM2_ONLY)
 	int power_mode = PM_MAX;
-#endif
 	/* wl_pkt_filter_enable_t	enable_parm; */
 	char iovbuf[32];
 	int bcn_li_dtim = DTIM_COUNT;
