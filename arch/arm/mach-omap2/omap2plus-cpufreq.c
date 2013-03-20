64a701f7e348b5fa7453117b3724dda806d56016
anarkia1976 <stefano.villa1976@gmail.com>
2013-03-20 01:05:57 +0100

    omap2plus-cpufreq.c: Set policy->max=1200000 - policy->min=350000 at Boot

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -354,8 +354,8 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = policy->cpuinfo.min_freq;
-	policy->max = policy->cpuinfo.max_freq;
+        policy->max = 1200000;
+        policy->min = 350000;
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
