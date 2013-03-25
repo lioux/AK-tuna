02887d85b5e167a47dfbf30ef76372f9168419c7
anarkia1976 <stefano.villa1976@gmail.com>
2013-03-26 00:35:13 +0100

    Kernel Version: AK.025

--- ak-build.sh
+++ ak-build.sh
@@ -46,7 +46,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="AK.023.DIAMOND"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="AK.025.DIAMOND"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
