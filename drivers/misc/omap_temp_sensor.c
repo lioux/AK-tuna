1b82ee7ae3266f4d32b18f8d4cf0e129ea39bb03
anarkia1976 <stefano.villa1976@gmail.com>
2013-03-28 13:28:01 +0100

    omap_temp_sensor.c + temp_control.c: Remove safety and Up the Threshold

--- drivers/misc/omap_temp_sensor.c
+++ drivers/misc/omap_temp_sensor.c
@@ -60,8 +60,8 @@ static void throttle_delayed_work_fn(struct work_struct *work);
 
 #define TSHUT_THRESHOLD_TSHUT_HOT	110000	/* 110 deg C */
 #define TSHUT_THRESHOLD_TSHUT_COLD	100000	/* 100 deg C */
-#define BGAP_THRESHOLD_T_HOT    80000  /* 80 deg C */
-#define BGAP_THRESHOLD_T_COLD   75000  /* 75 deg C */
+#define BGAP_THRESHOLD_T_HOT    90000  /* 80 deg C */
+#define BGAP_THRESHOLD_T_COLD   85000  /* 75 deg C */
 #define OMAP_ADC_START_VALUE	530
 #define OMAP_ADC_END_VALUE	923
 
