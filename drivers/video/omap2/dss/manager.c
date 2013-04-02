f21e6ed26a35f201eee6dc38bfd9498f477acb48
anarkia1976 <stefano.villa1976@gmail.com>
2013-04-02 09:38:45 +0200

    dss/manager.c: Fix for console debugging at boot

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -1767,8 +1767,9 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 
 	spin_lock_irqsave(&dss_cache.lock, flags);
 
-	if (!mgr->device || mgr->device->state != OMAP_DSS_DISPLAY_ACTIVE) {
-		pr_info_ratelimited("cannot apply mgr(%s) on inactive device\n",
+	// if (!mgr->device || mgr->device->state != OMAP_DSS_DISPLAY_ACTIVE) {
+	if (!mgr->device) {
+	pr_info_ratelimited("cannot apply mgr(%s) on inactive device\n",
 								mgr->name);
 		r = -ENODEV;
 		goto done;
